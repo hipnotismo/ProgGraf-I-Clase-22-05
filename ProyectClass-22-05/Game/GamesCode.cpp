e1ac885ab55a0dc45f80ab9299d2f2d942b8330b
Unknown <ale.bros@hotmail.com>
2018-05-22 18:01:39 -0300

    Window now oppens

--- ProyectClass-22-05/Game/GamesCode.cpp
+++ ProyectClass-22-05/Game/GamesCode.cpp
@@ -1,6 +1,29 @@
+#include <stdio.h>
+#include "allegro5\allegro.h"
+#include "allegro5\allegro_image.h"
 
+int main(int argc, char **argv) {
 
-void main()
-{
+	ALLEGRO_DISPLAY *display = NULL;
 
+	if (!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+	display = al_create_display(640, 480);
+	if (!display) {
+		fprintf(stderr, "failed to create display!\n");
+		return -1;
+	}
+
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	al_flip_display();
+
+	al_rest(2); // Time to close
+
+	al_destroy_display(display);
+
+	return 0;
 }
