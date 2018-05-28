256c88ba70596959278580b89c613a4e0d820ae9
Unknown <ale.bros@hotmail.com>
2018-05-28 20:04:54 -0300

    Image now appears on thw Window

--- ProyectClass-22-05/Game/GamesCode.cpp
+++ ProyectClass-22-05/Game/GamesCode.cpp
@@ -1,29 +1,58 @@
 #include <stdio.h>
-#include "allegro5\allegro.h"
-#include "allegro5\allegro_image.h"
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
 
 int main(int argc, char **argv) {
 
 	ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_BITMAP  *image = NULL;
+
+	display = al_create_display(800, 600);
+	image = al_load_bitmap("archivo de imagen.png");
+
+	bool playing = true;
+
+	// Initatializations
 
 	if (!al_init()) {
-		fprintf(stderr, "failed to initialize allegro!\n");
-		return -1;
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
 	}
 
-	display = al_create_display(640, 480);
+	if (!al_init_image_addon()) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
+	}
+
 	if (!display) {
-		fprintf(stderr, "failed to create display!\n");
-		return -1;
+		al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return 0;
 	}
 
-	al_clear_to_color(al_map_rgb(0, 0, 0));
+	if (!image) {
+		al_show_native_message_box(display, "Error", "Error", "Failed to load image!",
+			NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		al_destroy_display(display);
+		return 0;
+	}
 
-	al_flip_display();
+	// Initializations end
 
-	al_rest(2); // Time to close
+	al_draw_bitmap(image, 200, 200, 0);
+
+
+	while (playing) 
+	{
+		al_flip_display();
+	}
+	
 
 	al_destroy_display(display);
+	al_destroy_bitmap(image);
 
 	return 0;
 }
